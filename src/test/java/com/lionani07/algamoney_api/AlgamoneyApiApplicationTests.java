5b0caf64df808f9190bfa3970a2462669d235be8
Liorge Sanchez <lionani07@gmail.com>
2025-10-04 19:28:04 -0300

    Criando projeto

---
+++ src/test/java/com/lionani07/algamoney_api/AlgamoneyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lionani07.algamoney_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgamoneyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
