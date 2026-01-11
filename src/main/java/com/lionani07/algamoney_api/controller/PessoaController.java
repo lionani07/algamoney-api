ed8487bc5c37f2d38cb66ea9d486c1d1ca8a725e
Liorge Sanchez <lionani07@gmail.com>
2026-01-11 11:55:01 -0300

    Retornando pessoas

--- src/main/java/com/lionani07/algamoney_api/controller/PessoaController.java
+++ src/main/java/com/lionani07/algamoney_api/controller/PessoaController.java
@@ -6,10 +6,7 @@ import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import lombok.val;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
@@ -28,5 +25,11 @@ public class PessoaController {
                 .toUri();
 
         return ResponseEntity.created(location).body(pessoaCriada);
+    }
+
+    @GetMapping
+    public ResponseEntity<?> findAll() {
+        val pessoas = this.pessoaRepository.findAll();
+        return ResponseEntity.ok(pessoas);
     }
 }
