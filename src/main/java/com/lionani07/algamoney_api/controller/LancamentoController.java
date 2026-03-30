345ce4abad345b2da04448b188702dc1119a2aba
Liorge Sanchez <lionani07@gmail.com>
2026-03-29 22:31:21 -0300

    Find lancamento by codigo

--- src/main/java/com/lionani07/algamoney_api/controller/LancamentoController.java
+++ src/main/java/com/lionani07/algamoney_api/controller/LancamentoController.java
@@ -1,10 +1,12 @@
 package com.lionani07.algamoney_api.controller;
 
+import com.lionani07.algamoney_api.exception.AlgamoneyResourceNotFoundException;
 import com.lionani07.algamoney_api.repository.LancamentoRepository;
 import lombok.AllArgsConstructor;
 import lombok.val;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,5 +21,13 @@ public class LancamentoController {
     public ResponseEntity<?> findAll() {
         val lancamentos = this.lancamentoRepository.findAll();
         return ResponseEntity.ok(lancamentos);
+    }
+
+    @GetMapping("/{codigo}")
+    public ResponseEntity<?> findByCodigo(@PathVariable Long codigo) {
+        val lancamento = this.lancamentoRepository.findById(codigo)
+                .orElseThrow(() -> new AlgamoneyResourceNotFoundException("Lançamento não encontrado com o código: " + codigo));
+
+        return ResponseEntity.ok(lancamento);
     }
 }
